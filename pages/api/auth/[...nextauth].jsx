9f84d416a16b8842833e2887b732b15d154a0257
rabin-nyaundi <nyaundirabin@gmail.com>
2021-11-25 17:53:08 +0300

    fix

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -1,4 +1,4 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 // import prisma from "../../../lib/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
@@ -7,17 +7,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 let userAccount;
 
-export default NextAuth({
-
+const options = {
   session: {
     jwt: true,
     maxAge: 24 * 60 * 60,
+    strategy: "database",
   },
   
   cookie: {
     secure: process.env.NODE_ENV && process.env.NODE_ENV === 'production',
   },
-  
+
   providers: [
     CredentialsProvider({
       name: "credentials",
@@ -26,7 +26,7 @@ export default NextAuth({
           const user = await prisma.user.findUnique({
             where: { email: credentials.email },
           });
-  
+
           if (!user) {
             throw new Error("User with email not found");
           }
@@ -34,23 +34,23 @@ export default NextAuth({
             credentials.password,
             user.password
           );
-  
+
           if (!checkPassword) {
             throw new Error("Invalid password");
           }
-  
+
           userAccount = user;
           console.log(userAccount);
           return Promise.resolve(user);
-  
+
         } catch (error) {
           throw new Error(error);
         }
-  
+
       },
     }),
   ],
-  
+
   callbacks: {
     async session(session, token) {
       if (userAccount !== null) {
@@ -70,30 +70,28 @@ export default NextAuth({
       // }
       return Promise.resolve(session);
     },
-  
+
     async jwt({ token, user }) {
       const isSignedIn = user ? true : false;
-  
+
       if (isSignedIn) {
         token.accessToken = user.id.toString() + "-" + user.email + "-" + user.password;
       }
-  
+
       // if (user) {
       //   token.user = user
       // }
       return Promise.resolve(token);
     },
-  
+
   },
-  
+
   pages: {
     error: "/auth/login",
     signIn: "/auth/login",
     signOut: "/auth/login",
   },
-})
 
 
-
-
-// export default (req, res) => NextAuth(req, res, options);
+};
+export default (req, res) => NextAuth(req, res, options);
